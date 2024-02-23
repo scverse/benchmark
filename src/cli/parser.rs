235fb849fdd16c0101790bc58fcb5d6b8dd91ec3
Philipp A. <flying-sheep@web.de>
2024-02-23 12:25:34 +0100

    positional

--- src/cli/parser.rs
+++ src/cli/parser.rs
@@ -16,8 +16,10 @@ pub(crate) enum Commands {
 
 #[derive(Args)]
 pub(crate) struct ServeArgs {
-    #[arg(short, long, default_value = "0.0.0.0:3000")]
+    /// IP and port to listen on
+    #[arg(default_value = "0.0.0.0:3000")]
     pub(crate) addr: String,
+    /// Webhook secret as configured on GitHub
     #[arg(long, env)]
     pub(crate) secret_token: String,
 }
